8432697c76f414cceecdd4962e2eeeae89890374
Vishnu Najardhane <nurobloom.health@gmail.com>
2025-05-03 11:12:00 +0530

    Added template for home view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Welcome to NuroBloom!")
+def home(request):
+    return render(request, "home.html")
